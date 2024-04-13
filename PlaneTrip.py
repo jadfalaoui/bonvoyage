b5ec6de0f2136903a2a7b648577b527fb078c90e
Vincent Wu <1104vincentwu@gmail.com>
2024-04-13 17:10:09 -0400

    ah

--- PlaneTrip.py
+++ PlaneTrip.py
@@ -9,23 +9,24 @@ headers = {
 	"X-RapidAPI-Host": "skyscanner80.p.rapidapi.com"
 }
 
-inputBaseLocation = "Madrid"
-inputDepartureDate = "2024-11-04"
-inputFinalDestination = "Chicago"
-inputReturnDate = "2024-11-09"
+inputBaseLocation = "Chicago"
+inputDepartureDate = "2024-06-15"
+inputFinalDestination = "Madrid"
+inputReturnDate = "2024-06-26"
 
-inputAdultsGoing = 5
-inputCabinClassInt = 4
+inputAdultsGoing = 1
+inputCabinClassInt = 1
 inputCabinClass = ""
 
 if inputCabinClassInt == 1:
 	inputCabinClass = "economy"
-if inputCabinClass == 2:
+if inputCabinClassInt == 2:
 	inputCabinClass = "premium_economy"
-if inputCabinClass == 3:
+if inputCabinClassInt == 3:
 	inputCabinClass = "business"
-if inputCabinClass == 4:
+if inputCabinClassInt == 4:
 	inputCabinClass = "first"
+print(inputCabinClass)
 
 
 #url for API 
@@ -37,29 +38,40 @@ urlForPriceCalender = "https://skyscanner80.p.rapidapi.com/api/v1/flights/price-
 querystringAutoTrip = {"query":inputBaseLocation,"market":"US","locale":"en-US"}
 responsePlaneAutoComplete = requests.get(urlFlightAutoComplete, headers=headers, params=querystringAutoTrip)
 planeAutoComplete = responsePlaneAutoComplete.json()
-json_string = json.dumps(planeAutoComplete)
-data = json.loads(json_string)
-baseLocation = data['data'][0]['id']
+json_stringPlaneAutoComplete = json.dumps(planeAutoComplete)
+data1 = json.loads(json_stringPlaneAutoComplete)
+baseLocation = data1['data'][0]['id']
 
 #destination location ID generator 
 querystringAutoTrip = {"query":inputFinalDestination,"market":"US","locale":"en-US"}
 responsePlaneAutoComplete = requests.get(urlFlightAutoComplete, headers=headers, params=querystringAutoTrip)
 planeAutoComplete = responsePlaneAutoComplete.json()
-json_string = json.dumps(planeAutoComplete)
-data = json.loads(json_string)
-destinationLocation = data['data'][0]['id']
+json_stringPlaneAutoComplete = json.dumps(planeAutoComplete)
+data1 = json.loads(json_stringPlaneAutoComplete)
+destinationLocation = data1['data'][0]['id']
 
-
-
+#round Trip
 querystringForTrip = {"fromId":baseLocation,"toId":destinationLocation,"departDate":inputDepartureDate,"returnDate":inputReturnDate,"adults":inputAdultsGoing,"cabinClass":inputCabinClass,"currency":"USD","market":"US","locale":"en-US"}
 responseForTrip = requests.get(urlForRoundTrip, headers=headers, params=querystringForTrip)
-print(responseForTrip.json())
+roundTrip = responseForTrip.json()
+json_stringRoundTrip = json.dumps(roundTrip)
+data1 = json.loads(json_stringRoundTrip)
+roundTripToken = data1['data']['token']
+print(roundTripToken)
+print(data1['data']['itineraries'])
+roundTripSessionId = data1['data']['itineraries'][0]['price']['formatted']
+print(roundTripSessionId)
+
+
+
+
+
+
 
 #Price generator
-querystring = {"fromId":baseLocation,"toId":destinationLocation,"departDate":inputDepartureDate,"returnDate":inputReturnDate,"currency":"USD","market":"US","locale":"en-US"}
-response = requests.get(urlForPriceCalender, headers=headers, params=querystring)
-print(response.json())
-
+#querystring = {"fromId":baseLocation,"toId":destinationLocation,"departDate":inputDepartureDate,"returnDate":inputReturnDate,"currency":"USD","market":"US","locale":"en-US"}
+#response = requests.get(urlForPriceCalender, headers=headers, params=querystring)
+#print(response.json())
 
 
 

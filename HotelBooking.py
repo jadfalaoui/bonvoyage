d264f1d80e4918ece0287fa1a2ca47cd52afb7aa
Vincent Wu <1104vincentwu@gmail.com>
2024-04-14 10:18:44 -0400

    New

--- HotelBooking.py
+++ HotelBooking.py
@@ -1,14 +1,48 @@
 import requests
+import json
+import requests
 
-url = "https://skyscanner80.p.rapidapi.com/api/v1/hotels/prices"
+urlForAutoComplete = "https://skyscanner80.p.rapidapi.com/api/v1/hotels/auto-complete"
+urlForHotelPrices = "https://skyscanner80.p.rapidapi.com/api/v1/hotels/prices"
+urlForSearch = "https://skyscanner80.p.rapidapi.com/api/v1/hotels/search"
 
-querystring = {"id":"218005414","checkin":"<REQUIRED>","checkout":"<REQUIRED>","rooms":"1","adults":"2","currency":"USD","market":"US","locale":"en-US"}
+
+destinationLocation = "London"
+checkInDate = "2024-06-16"
+checkOutDate = "2024-06-26"
+roomNumber = 4
+adultsComing = 5
+priceTotal = "PRICE_TOTAL"
+starsWantedForHotel = 4
 
 headers = {
 	"X-RapidAPI-Key": "fa3ebe3994mshf4255cceae92845p1c034ajsn1048793cb092",
 	"X-RapidAPI-Host": "skyscanner80.p.rapidapi.com"
 }
 
-response = requests.get(url, headers=headers, params=querystring)
+#For Auotcomplete
+querystringForAutoComplete = {"query":destinationLocation,"market":"US","locale":"en-US"}
+stringForAutoComplete = requests.get(urlForAutoComplete, headers=headers, params=querystringForAutoComplete)
+forAutoComplete = stringForAutoComplete.json()
+json_stringForAutoComplete = json.dumps(forAutoComplete)
+data1 = json.loads(json_stringForAutoComplete)
+baseLocationEntityId = data1['data'][0]['entityId']
 
-print(response.json())
+#For Search
+querystringForHotelSearch = {"entityId":baseLocationEntityId,"checkin":checkInDate,"checkout": checkOutDate,"rooms":roomNumber,"adults":adultsComing,"resultsPerPage":"1","page":"1","priceType":"PRICE_TOTAL","sorting":"-stars","stars":starsWantedForHotel,"currency":"USD","market":"US","locale":"en-US"}
+stringForHotelSearch = requests.get(urlForSearch, headers=headers, params=querystringForHotelSearch)
+forHotelSearch = stringForHotelSearch.json()
+json_stringForHotelSearch = json.dumps(forHotelSearch)
+data1 = json.loads(json_stringForHotelSearch)
+hotelName = data1['data']['results']['hotelCards'][0]['name']
+
+#ForRelativeDistance
+hotelRelativeDistance = data1['data']['results']['hotelCards'][0]['distance']
+
+#For Price
+hotelPrice = data1['data']['results']['hotelCards'][0]['lowestPrice']['price']
+
+hotelInformation = [hotelName, hotelPrice, hotelRelativeDistance]
+
+print(hotelInformation)
+

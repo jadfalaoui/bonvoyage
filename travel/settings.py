5eae92c579130d099152b21430d184558b03f2b6
jad <alaoui@umich.edu>
2024-04-14 04:07:23 -0400

    paths

--- travel/settings.py
+++ travel/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'travel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR / 'templates')],
+        'DIRS': [BASE_DIR+'/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

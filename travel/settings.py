d406fe7f9205caa4b0fd05a37997cfcb0e578e2c
jad <alaoui@umich.edu>
2024-04-14 04:08:54 -0400

    paths

--- travel/settings.py
+++ travel/settings.py
@@ -74,15 +74,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'travel.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
